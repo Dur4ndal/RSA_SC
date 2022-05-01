b03b1bb80624b4ce9c6eb94b2fa157ebe3a90a8a
Xavier-Edups <eduardoxdantas@gmail.com>
2022-05-01 00:43:25 -0300

    OAEP final

--- OAEP.py
+++ OAEP.py
@@ -1,25 +1,52 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-G = 10
-H = 15
+import secrets
+import hashlib
 
-def pad(a, size):
-   return [i * 2 ** (size - i.bit_length()) for i in a]
+G = 1024
+H = 512
+
+def pad(m, size):
+    bit_size = len(m) * 8
+    assert bit_size <= size
+    ba = bytearray(m)
+    for i in range(int((size - bit_size) / 8)):
+        ba.append(0)
+    return ba
 
 def G_hash(r):
-    return pad(r, G)
-
+    g = hashlib.sha1()
+    g.update(r)
+    x = g.digest() #Size = 160
+    rem = int(G / 8)
+    if G <= 160:
+        return x[:rem]
+    else: #G >160 
+        while len(x) < G:
+            x += x
+        return x[:rem]
 def H_hash(x):
-    return pad(x, H)
+    h = hashlib.md5()
+    h.update(x)
+    y = h.digest() #Size = 128
+    rem = int(H / 8)
+    if H <= 128:
+        return y[:rem]
+    else: #H > 128 
+        while len(y) < H:
+            y += y
+        return y[:rem]
 
 def xor(a, b):
-    assert len(a) == len(b)
+    assert len(bytearray(a)) == len(bytearray(b))
     return [a[i] ^ b[i] for i in range(len(a))]
 
-def encrypt_key(m1, r):
-    x = xor(pad(m1, G), G_hash(r))
-    y = xor(pad(r, H), H_hash(x))
-    return x+y
+def oaep_encrypt(m1): 
+    b_m1 = pad(m1.encode(), G)
+    b_r = bytearray(secrets.token_bytes(int(H / 8)))
+    x = xor(b_m1, G_hash(b_r))
+    y = xor(b_r, H_hash(bytearray(x)))
+    return itos(x+y)
 
 def stoi(s):
     return [ord(c) for c in s]
@@ -27,13 +54,3 @@ def stoi(s):
 def itos(a):
     return ''.join(chr(i) for i in a)
 
-m1 = 'mamamama'
-r = 'maglione'
-
-b_m1 = stoi(m1)
-b_r = stoi(r)
-
-b_key = encrypt_key(b_m1, b_r)
-
-print([b.bit_length() for b in b_key])
-print(itos(b_key).encode('utf-8'))
